917c2ffc706c842d6f00e8d83d42fa37d0291c05
Pingkanzhr <adeliapingkan112@gmail.com>
2022-01-03 19:50:47 -1200

    Add project files.

---
+++ ServerConfigRest_001_AdeliaPingkanAzzahra/Program.cs
@@ -0,0 +1,33 @@
+﻿using ServiceRest_001_AdeliaPingkanAzzahra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerConfigRest_001_AdeliaPingkanAzzahra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server Ready");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
